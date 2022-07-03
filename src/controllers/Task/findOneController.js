d1f340a6c928490c1faffab39076220c2e7f6ef2
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-07-03 13:52:19 +0800

    implemented an API for getting a task

---
+++ src/controllers/Task/findOneController.js
@@ -0,0 +1,14 @@
+const Task = require('@models/Task');
+
+const findOneController = async (req, res) => {
+  try {
+    const task = await Task.findAll({ where: { id: req.params.taskId } });
+    res.status(200).json(task[0] || {});
+  } catch(error) {
+    res.status(500).json({
+      message: `Find a task error: ${error}`
+    });
+  }
+}
+
+module.exports = findOneController;
--- src/controllers/Task/index.js
+++ src/controllers/Task/index.js
@@ -1,7 +1,9 @@
 const createController = require('./createController');
 const updateController = require('./updateController');
+const findOneController = require('./findOneController');
 
 module.exports = {
   createController,
   updateController,
+  findOneController,
 }
