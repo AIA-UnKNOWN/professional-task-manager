9f879ed972c0623c7d3a33dfd8fd9d5b89248cb4
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-07-17 02:23:47 +0800

    added migration for adding foreign key constraints in task table

--- src/migrations/20220716171731-add-foreign-key-constraints-for-task-table.js
+++ src/migrations/20220716171731-add-foreign-key-constraints-for-task-table.js
@@ -8,6 +8,44 @@ module.exports = {
      * Example:
      * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
      */
+    const columns = [
+      {
+        column: 'project_id',
+        constraint: {
+          type: Sequelize.DataTypes.INTEGER,
+          references: {
+            model: 'Projects',
+            key: 'id',
+            deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+          }
+        }
+      },
+      {
+        column: 'label_id',
+        constraint: {
+          type: Sequelize.DataTypes.INTEGER,
+          references: {
+            model: 'Labels',
+            key: 'id',
+            deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+          }
+        }
+      },
+      {
+        column: 'priority_id',
+        constraint: {
+          type: Sequelize.DataTypes.INTEGER,
+          references: {
+            model: 'Priorities',
+            key: 'id',
+            deferrable: Sequelize.Deferrable.INITIALLY_IMMEDIATE
+          }
+        }
+      }
+    ];
+    columns.forEach(async ({ column, constraint }) => {
+      await queryInterface.changeColumn('Tasks', column, constraint);
+    });
   },
 
   async down (queryInterface, Sequelize) {
@@ -17,5 +55,22 @@ module.exports = {
      * Example:
      * await queryInterface.dropTable('users');
      */
+     const columns = [
+      {
+        column: 'project_id',
+      },
+      {
+        column: 'label_id',
+      },
+      {
+        column: 'priority_id',
+      }
+    ];
+    columns.forEach(async ({ column }) => {
+      await queryInterface.changeColumn('Tasks', column, {
+        type: Sequelize.DataTypes.INTEGER,
+        references: null
+      });
+    });
   }
 };
