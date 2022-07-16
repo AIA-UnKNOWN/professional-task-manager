54ed8bd868909d4cac959de7935d48896de533f7
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-07-17 01:23:21 +0800

    added migrations for creating and modifying models

---
+++ src/migrations/20220716170854-create-task.js
@@ -0,0 +1,42 @@
+'use strict';
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Tasks', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      project_id: {
+        type: Sequelize.INTEGER
+      },
+      label_id: {
+        type: Sequelize.INTEGER
+      },
+      priority_id: {
+        type: Sequelize.INTEGER
+      },
+      title: {
+        type: Sequelize.STRING
+      },
+      description: {
+        type: Sequelize.STRING
+      },
+      is_completed: {
+        type: Sequelize.BOOLEAN
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Tasks');
+  }
+};
