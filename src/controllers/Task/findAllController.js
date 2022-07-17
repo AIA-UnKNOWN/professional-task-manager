e737f83760c8c19a8cdc7fb3fb854597caa7aae6
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-07-17 22:51:59 +0800

    fixed /tasks route not getting request queries

--- src/controllers/Task/findAllController.js
+++ src/controllers/Task/findAllController.js
@@ -2,7 +2,12 @@ const { Task } = require('@models');
 
 const findAllController = async (req, res) => {
   try {
-    const tasks = await Task.findAll({ where: { project_id: req.body.project_id } });
+    console.log(req.params)
+    const tasks = await Task.findAll({
+      where: {
+        project_id: req.query.projectId
+      }
+    });
     res.status(200).json(tasks);
   } catch(error) {
     res.status(500).json({
