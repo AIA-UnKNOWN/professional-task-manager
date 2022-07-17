1a52b4b9b54b6ef54735d01b9bb6a11cc39614de
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-07-17 22:29:45 +0800

    fixed cors issue for frontend

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('module-alias/register');
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const PORT = 3000;
 
 const registerRoutes = require('@routes');
 
+app.use(cors());
 app.use(express.json());
 
 registerRoutes(app);
