6c0b9c39218bb10fc72b56ebdd56ba47517fa4c7
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-07-24 20:13:41 +0800

    create task api now returns a newly created task as a response

--- src/controllers/Task/createController.js
+++ src/controllers/Task/createController.js
@@ -3,14 +3,14 @@ const { Task } = require('@models');
 const createController = async (req, res) => {
   const { title, description, project_id, label_id, priority_id } = req.body;
   try {
-    await Task.create({
+    const newlyCreatedTask = await Task.create({
       title,
       description: description || null,
       project_id,
       label_id:label_id || null,
       priority_id: priority_id || null
     });
-    res.sendStatus(201);
+    res.status(201).json(newlyCreatedTask);
   } catch(error) {
     res.status(500).json({
       message: `Task creation error: ${error}`
