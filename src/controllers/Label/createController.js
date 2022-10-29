d625588577806207fdb9b4321cce6bedf628d352
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-10-29 08:43:26 -0400

    create label api returns data after created

--- src/controllers/Label/createController.js
+++ src/controllers/Label/createController.js
@@ -2,8 +2,8 @@ const { Label } = require('@models');
 
 const createController = async (req, res) => {
   try {
-    await Label.create({ name: req.body.name });
-    res.sendStatus(201);
+    const newlyCreatedLabel = await Label.create({ name: req.body.name });
+    res.status(201).json(newlyCreatedLabel);
   } catch(error) {
     res.status(500).json({
       message: `Label create error: ${error}`
