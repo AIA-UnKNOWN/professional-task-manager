b0385005dac2a831d04d42f8852aa619b44f35ff
Ajboy Ian Abordo <ajboyian321@gmail.com>
2022-10-30 07:03:06 -0400

    find all projects api returns corresponding tasks

--- src/controllers/Project/findAllController.js
+++ src/controllers/Project/findAllController.js
@@ -1,8 +1,13 @@
-const { Project } = require('@models');
+const { Project, Task } = require('@models');
 
 const findAllController = async (req, res) => {
   try {
-    const projects = await Project.findAll();
+    const projects = await Project.findAll({
+      include: {
+        model: Task,
+        as: 'tasks',
+      }
+    });
     res.status(200).json(projects);
   } catch(error) {
     res.status(500).json({
