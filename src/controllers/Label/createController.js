6ba5538d4a41a09cece6bf62e347d03795a6820b
Ajboy Ian Abordo <ajboyian321@gmail.com>
2023-03-12 19:27:47 +0800

    return created label with associated tasks

--- src/controllers/Label/createController.js
+++ src/controllers/Label/createController.js
@@ -1,9 +1,16 @@
-const { Label } = require('@models');
+const { Label, Task } = require('@models');
 
 const createController = async (req, res) => {
   try {
     const newlyCreatedLabel = await Label.create({ name: req.body.name });
-    res.status(201).json(newlyCreatedLabel);
+    const label = await Label.findOne({
+      where: { id: newlyCreatedLabel.id },
+      include: {
+        model: Task,
+        as: 'tasks',
+      },
+    })
+    res.status(201).json(label);
   } catch(error) {
     res.status(500).json({
       message: `Label create error: ${error}`
